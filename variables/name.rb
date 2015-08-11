183d3a8f68c5ea251316dc7a6546e4113ab073da
renjuzac <renjuzac@Renjus-MacBook-Pro.local>
2015-08-10 20:40:05 -0700

    Methods exercise

--- variables/name.rb
+++ variables/name.rb
@@ -1,3 +1,7 @@
 puts "Enter Name"
 value=gets.chomp
 puts "Greetings #{value}"
+
+10.times do
+puts value
+end
---
+++ variables/names4.rb
@@ -0,0 +1,6 @@
+puts "Enter First Name"
+fname=gets.chomp
+puts "Enter Last Name"
+lname=gets.chomp
+
+puts "#{fname} #{lname}"
